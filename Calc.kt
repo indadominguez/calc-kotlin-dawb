4dca648b05cbbd4b10c2f92faa19965211263ad1
indadominguez <dhindalecio@iesrafaelalberti.es>
2025-03-12 16:50:46 +0100

    Update Calc.kt

--- Calc.kt
+++ Calc.kt
@@ -1,7 +1,13 @@
+fun divideE(num1: Int, num2: Int): Int {
+    return num1 / num2
+}
+
+
 fun main(args: Array) {
     val (operation, num1, num2) = parseArguments(args)
     val result = when (operation) {
         "multiplica" -> multiplica(num1, num2)
+        "divide" -> divideE(num1, num2)
         else -> throw IllegalArgumentException("Operación no soportada")
     }
     showResult(operation, result)
